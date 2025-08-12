88a97a9a2caba2c1b8b221e4c5c5d120b19f4eed
kalaimaran18 <kalaimaranr18@gmail.com>
2025-08-12 22:43:44 +0530

    Add files via upload

---
+++ class and objects.py
@@ -0,0 +1,71 @@
+#Create a Person class with attributes name and age. Write a method to display the person's details.
+
+#Class and object (Problem 1)
+
+class Person:
+    def name(self):
+        print("kalai")
+    def age(self):
+        print("20")
+
+e1=Person()
+e1.age()
+e1.name()
+
+#Define a Car class with attributes brand, model, and year. Create an object of the class and print its attributes.
+
+# class and object (Problem 2)
+
+class Car:
+    def brand(self):
+        print("Audi")
+    def model(self):
+        print("A4371")
+
+d1=Car()
+d1.brand()
+d1.model()
+
+#Create a Rectangle class with attributes length and width. Write methods to calculate the area and perimeter.
+
+# Class and object (problem 3)
+
+class rectanggle:
+    def length(self):
+        print("area of rectangle")
+    def width(self):
+        print("Sum of perimeter")
+
+s1=rectanggle()
+s1.length()
+s1.width()
+
+#Define a Student class with attributes name, roll_number, and grade. Create an object and print its attributes.
+
+# Class and object (Problem 4)
+
+class student:
+    def name(self):
+        print("AAAAA")
+    def roll_number(self):
+        print("43")
+
+f1=student()
+f1.name()
+f1.roll_number()
+
+#Create a Book class with attributes title, author, and price. Write a method to display the book's details.
+
+# Class and object (Problem 5)
+class book:
+    def title(self):
+        print("wings of fire")
+    def author(self):
+        print("APJ Abdul kalam")
+    def price(self):
+        print("360")
+
+w1=book()
+w1.title()
+w1.author()
+w1.price()
